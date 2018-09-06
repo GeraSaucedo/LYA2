c17ee8eaf1239124e7d5efc07fc8b54472f52711
Gerardo Saucedo <jegsg@hotmail.es>
2018-09-06 11:28:04 -0500

    Update main, casi terminado
    Hacer mas pruebas, falta el agregar el caracter de escape

--- src/Lexico/Main.java
+++ src/Lexico/Main.java
@@ -11,48 +11,72 @@ public class Main {
 		//Leemos el archivo
 		try{
 			
-	         FileInputStream FIS = new FileInputStream("C:\\Users\\Jerry\\Desktop\\LEXICO.txt");  //Abrimos el archivo
+	         FileInputStream FIS = new FileInputStream("C:\\Users\\Training\\Desktop\\Lex.txt");  //Abrimos el archivo
 	         DataInputStream DIS = new DataInputStream(FIS); //se crea el objeto de entrada
 	         BufferedReader buffer = new BufferedReader(new InputStreamReader(DIS)); //se crea buffer de lectura
-	         String strLinea;
-	         List lista = new ArrayList();
-	         List listaDel = new ArrayList();
+	         String strLinea, caracter = ""; //CARACTER declarado para cuando los delimitadores vengan pegados
+	         boolean juntos = false;
+	         List lista = new ArrayList(); // guadamos los que no son delimitadores
+	         List listaDel = new ArrayList(); //guardamos delimitadores
 	         
-	         int linea = 0;
-	         Token token = new Token();
+	         int linea = 0; //para contar las lineas
+	         Token token = new Token(); 
 	        
 	         // Leer el archivo linea por linea
-	         while ((strLinea = buffer.readLine()) != null)   {
-	             linea++;
-	             String cadenaDel = "";
+	         while ((strLinea = buffer.readLine()) != null)   { //while tenga lineas para leer  seguira leyendo
+	             linea++; // indica la linea en la que esta leyendo util para la impresion
+	             String cadenaDel = ""; // guardamos la cadena de los delimitadores en la variable para ocupar menos memoria
 	             for(int i=0;i<strLinea.length();i++) { // FOR PARA LEER CADA CARACTER DE LA LINEA
 	            	 
-	            	if(token.isDelimitadorToken(strLinea.charAt(i)) || token.isDelimitadorNoToken(strLinea.charAt(i))) {
+	            	if(token.isDelimitadorToken(strLinea.charAt(i)) || token.isDelimitadorNoToken(strLinea.charAt(i))) { //checamos que si es delimimtador o no
 	            		
-	            		if(token.isDelimitadorToken(strLinea.charAt(i))) {
+	            		if(token.isDelimitadorToken(strLinea.charAt(i))) { //si el caracter que llego genera token se guarda en la lista de los delimitadores
 	            			listaDel.add(strLinea.charAt(i)); //AGREGAMOS DELIMITADOR A LA LISTA
 	            		}
-	            		else {	
-	            			cadenaDel = token.imprimirLista(listaDel); //guardamos la variable para no consumir tanta memoria
-	            			listaDel.clear();
-	            			if(token.isOperador(cadenaDel)) System.out.println("Operador " + token.getTipoOperador(cadenaDel) + ": " + cadenaDel + " Linea " + linea);
-	            			if(token.isSpecialCharacter(cadenaDel)) System.out.println("Caracter especial:  " + cadenaDel + " Linea " + linea);
+	            		else {	// SI NO se guarda la lista en la variable cadena del para que despues sea comparado he impreso
+	            			
+	            			cadenaDel = token.imprimirLista(listaDel); 
+	            			listaDel.clear(); //Limpiamos la lista para que no se junten los valores a comparar
+	            				            			
+	            			
+	            			if(token.isOperador(cadenaDel)) {
+	            				System.out.println("Operador " + token.getTipoOperador(cadenaDel) + ": " + cadenaDel + " Linea " + linea);
+	            			}
+	            			else if(token.isSpecialCharacter(cadenaDel)) { 
+	            				System.out.println("Caracter especial:  " + cadenaDel + " Linea " + linea);
+	            			}else {
+	            				
+	            				//Si no fue operador ni caracter especial significa que llegaron simbolos pegados por lo que hay que identificarlos con  el siguiente for
+	            				for(int k=0;k<cadenaDel.length();k++) {
+	            
+	            					caracter = ""; //reinicializamos para que no se junten de nuevo los caracterers a verificar si son operadores o caracteres especiales 
+	            					caracter += cadenaDel.charAt(k); //caracter a comparar
+	            					
+	            					if(token.isOperador(caracter)) System.out.println("Operador " + token.getTipoOperador(caracter) + ": " + caracter + " Linea " + linea);
+		            				if(token.isSpecialCharacter(caracter)) System.out.println("Caracter especial:  " + caracter + " Linea " + linea);
+		            			}
+	            				caracter = "";//para que no se repita al final, es decir, que no se quede ninguno
+	            				
+	            			}
 	            		}
 	            		
+	            		//SI el delimitador que llego no genera token procedemos a comparar lo que se guardo en la lista 
+	            			            		
 	            		String cadena = token.imprimirLista(lista); // guardamos la variable para no consumir tanta memoria
 	            		
 	            		if(token.isPalabraReservada(cadena)) System.out.println("Palabra reservada: " + cadena + " Linea " + linea);
 	            		if(token.isNumeroEntero(cadena)) System.out.println("Numero entero:  " + cadena + " Linea " + linea);
 	            		if(token.isNumeroReal(cadena)) System.out.println("Numero Real:  " + cadena + " Linea " + linea);
 	            		if(!lista.isEmpty() && token.isString(cadena)) System.out.println("Constante string:  " + cadena + " Linea " + linea);
-	            		if(token.isComentario(cadena)) {
+	            		if(token.isIdentificador(cadena) && !token.isPalabraReservada(cadena)) System.out.println("Identificador:  " + cadena + " Linea " + linea); 
+		            	if(token.isComentario(cadena)) {
 	            			System.out.println("Comentario:  " + cadena + "... Linea " + linea);
 	            			i = strLinea.length(); //Terminamos de leer la linea porque fue un comentario, es decir, lo siguiente cuenta como el mismo comentario
 	            		}
 	            		lista.clear(); 
 	            	} 
-	            	else {
-	            		lista.add(strLinea.charAt(i));
+	            	else { //No fue delimitador  
+	            		lista.add(strLinea.charAt(i)); //el caracter se agregara a la lista para despues ser evaluado
 	            		cadenaDel = token.imprimirLista(listaDel); //guardamos la variable para no consumir tanta memoria
             			listaDel.clear();
             			if(token.isOperador(cadenaDel)) System.out.println("Operador " + token.getTipoOperador(cadenaDel) + ": " + cadenaDel + " Linea " + linea);
@@ -62,16 +86,31 @@ public class Main {
 	            
 	             }//Cerrar for de leer caracteres de la linea
 	             
+	             //Si quedo algo en las listas debe ser impreso ya que no llegara delimitador de EOLN
+	             
 	             if(!lista.isEmpty() || !listaDel.isEmpty()) { //si las listas no estan vacias al terminar de leer la linea se imprime lo que hay dentro 
 	            	 String cadena = token.imprimirLista(lista);
 	            	 String cadenaDelim = token.imprimirLista(listaDel);
+	            	 
 	            	 if(token.isPalabraReservada(cadena)) System.out.println("Palabra reservada: " + cadena + " Linea " + linea);
-	            	 if(token.isOperador(cadenaDelim)) System.out.println("Operador " + token.getTipoOperador(cadenaDelim) + ": " + cadenaDelim + " Linea " + linea);
-	            	 if(token.isNumeroEntero(cadena)) System.out.println("Numero entero:  " + cadena + " Linea " + linea);
-	            	 if(token.isNumeroReal(cadena)) System.out.println("Numero Real:  " + cadena + " Linea " + linea);
-	            	 if(token.isSpecialCharacter(cadenaDelim)) System.out.println("Caracter especial:  " + cadenaDelim + " Linea " + linea);
-	            	 if(!lista.isEmpty() && token.isString(cadena)) System.out.println("Constante string:  " + cadena + " Linea " + linea);
-	            	 if(token.isComentario(cadena)) System.out.println("Comentario:  " + cadena + " Linea " + linea);
+	            	 if(token.isOperador(cadenaDelim))  System.out.println("Operador " + token.getTipoOperador(cadenaDelim) + ": " + cadenaDelim + " Linea " + linea); 
+	            	 if(token.isNumeroEntero(cadena)) System.out.println("Numero entero:  " + cadena + " Linea " + linea);  
+	            	 if(token.isNumeroReal(cadena)) System.out.println("Numero Real:  " + cadena + " Linea " + linea); 
+	            	 if(token.isSpecialCharacter(cadenaDelim))  System.out.println("Caracter especial:  " + cadenaDelim + " Linea " + linea);
+	            	 if(!lista.isEmpty() && token.isString(cadena))  System.out.println("Constante string:  " + cadena + " Linea " + linea); 
+	            	 if(token.isComentario(cadena))  System.out.println("Comentario:  " + cadena + " Linea " + linea); 
+	            	 if(token.isIdentificador(cadena) && !token.isPalabraReservada(cadena))  System.out.println("Identificador:  " + cadena + " Linea " + linea);
+	            	 if(!token.isOperador(cadenaDelim)) { //SI llegaron delimitadores juntos 
+	            		 for(int k=0;k<cadenaDelim.length();k++) {
+	            			 caracter = ""; //reinicializamos para que no se junten de nuevo los caracterers a verificar si son operadores o caracteres especiales 
+	            			 caracter += cadenaDelim.charAt(k); //caracter a comparar
+     					
+	            			 if(token.isOperador(caracter)) System.out.println("Operador " + token.getTipoOperador(caracter) + ": " + caracter + " Linea " + linea);
+	            			 if(token.isSpecialCharacter(caracter)) System.out.println("Caracter especial:  " + caracter + " Linea " + linea);
+	            		 }
+	            	 }
+	            	 
+	            	 
 	            }//END if lista empty
 	            
 			lista.clear(); //Limpiamos la lista por el EOLN
